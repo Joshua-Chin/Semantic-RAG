3af30fa7c3d169a7d1bbd39978cbaac7ff31d5a0
Joshua Chin <5422226+Joshua-Chin@users.noreply.github.com>
2025-08-30 19:44:32 -0700

    Correct metrics.py

--- lib/metrics.py
+++ lib/metrics.py
@@ -1,3 +1,29 @@
+import torch
+
+
+def evaluate_rag(benchmark, documents, similarities, topk):
+    precision = recall = 0
+    count = 0
+    indices = torch.argsort(similarities, descending=True)[:, :topk]
+    for test_idx, document_idxs in enumerate(indices):
+        # Compute spans
+        spans_true = []
+        for snippet in benchmark[test_idx]["snippets"]:
+            spans_true.append(snippet["span"])
+        spans_pred = []
+        for idx in document_idxs:
+            document = documents[idx]
+            start = document.metadata["start_index"]
+            length = len(document.page_content)
+            spans_pred.append((start, start + length))
+        # Compute precision and recall
+        p, r = precision_recall(spans_true, spans_pred)
+        # Update accumulators
+        precision += p
+        recall += r
+        count += 1
+    return precision / count, recall / count
+
 def precision_recall(spans_true, spans_pred):
     indices_true = as_indices(spans_true)
     indices_pred = as_indices(spans_pred)
